664711bec3720d06dd08d2b6271eba325642c4db
Vadim Ilinchik <ivp.dev@outlook.com>
2022-11-06 17:38:08 +0300

    add persist context to public api

--- packages/redux-dynamic-modules-persist-react/src/index.ts
+++ packages/redux-dynamic-modules-persist-react/src/index.ts
@@ -1,2 +1,3 @@
 export { default as ReduxGate } from "./ReduxGate";
 export { default as usePersistContext } from "./usePersistContext";
+export { default as PersistContext } from "./PersistContext";
